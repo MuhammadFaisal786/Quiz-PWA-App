15bf07efe9b234c0668aff88041982c5430015db
muhammadfaisal <faisalmfaisal015@gmail.com>
2020-09-14 16:23:14 +0500

    first commit

---
+++ src/Services/swRegister.tsx
@@ -0,0 +1,8 @@
+export default function swDev(){
+   
+  
+    let swUrl = `${process.env.PUBLIC_URL}/serviceWorker.js`
+    navigator.serviceWorker.register(swUrl).then((result) => console.log(result) )
+    
+  
+  }
